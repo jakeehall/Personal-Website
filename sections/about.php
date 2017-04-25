9633007e7344cd8540cd7c70c384672a0ea7b52b
Jake Hall <jakeehall@gmail.com>
2017-04-25 15:02:03 -0600

    Preparing to add about section

--- sections/about.php
+++ sections/about.php
@@ -1,6 +1,5 @@
 <h1 style="background-image: url(./images/headers/about.jpg);">About</h1>
 <content>
-  <p>This website is (obviously) under re-development. In fact I'm really embarresed to even have this as my temporary website. Please check back soon for updates!</p>
-  <h2>Sub Heading Test</h2>
-  <p>more text lol... the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog.</p>
+  <h2>Hello World!</h2>
+  <p></p>
 </content>
